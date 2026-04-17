67e2496c4ec6507b243ddebf2717a9bbfad2cb88
Jyothi Swaroop Marri <jyothiswaroop.marri@pal.tech>
2026-04-17 09:49:23 +0530

    commit3

--- backend/Controllers/HelloController.cs
+++ backend/Controllers/HelloController.cs
@@ -1,15 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace backend.Controllers
+[ApiController]
+[Route("api/[controller]")]
+public class HelloController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class HelloController : ControllerBase
+    [HttpGet]
+    public IActionResult Get()
     {
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok(new { message = "Hello from .NET Backend 🚀" });
-        }
+        return Ok(new { message = "Hello from .NET Backend 🚀" });
     }
 }
--- backend/Program.cs
+++ backend/Program.cs
@@ -5,18 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        p => p.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader());
-});
-
 var app = builder.Build();
-
-app.UseCors("AllowAll");
 
 app.MapControllers();
 
-app.Run();
+app.Run("http://0.0.0.0:5000");
