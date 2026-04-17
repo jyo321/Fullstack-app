a1408ef230cfb245cbd281255d3d12cc1b80f21f
Jyothi Swaroop Marri <jyothiswaroop.marri@pal.tech>
2026-04-17 10:32:42 +0530

    commit5

--- backend/Program.cs
+++ backend/Program.cs
@@ -12,3 +12,19 @@ app.MapControllers();
 app.Run("http://0.0.0.0:5000");
 
 builder.Services.AddHostedService<LoadGenerator>();
+
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing =>
+    {
+        tracing
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("dotnet-backend"))
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(opt =>
+            {
+                opt.Endpoint = new Uri("http://otel-collector:4317");
+            });
+    });
